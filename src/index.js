74664c6841b9b196def5e9c8632d1636b3383cf6
Robert P. <porobertdev@gmail.com>
2024-02-28 19:44:50 +0200

    Add module for about page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ console.log('testing webpack');
 import Home from './navbar/home.js';
 import Menu from './navbar/menu.js';
 import Contact from './navbar/contact.js';
+import About from './navbar/about.js';
 
 const content = document.querySelector('#content');
 
 // for testing till I add event listeners
-content.innerHTML = Contact();
+content.innerHTML = About();
---
+++ src/navbar/about.js
@@ -0,0 +1,17 @@
+export default function () {
+    const html = `
+        <p>
+            In the heart of Tokyo, chef Akira's love for authentic ramen inspired him to create Ramen Delight.
+        </p>
+        <p>
+            With recipes passed down from generations, he opened our doors, offering savory bowls of tradition and innovation. From Tokyo to our vibrant city, we've become a beloved spot for friends and family to savor the warmth of good food and lasting memories.
+        </p>
+        
+        <p>
+            Welcome to Ramen Delight, where every slurp tells a tale of passion and flavor.
+        </p>
+    `;
+
+    content.classList.add('about');
+    return html;
+}
