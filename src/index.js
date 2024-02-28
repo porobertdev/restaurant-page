1ddc3e88988b67b6649146fc1e102763991a9459
Robert P. <porobertdev@gmail.com>
2024-02-28 18:51:00 +0200

    Add module for home tab

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./style.css";
 console.log('testing webpack');
+
+import Home from './navbar/home.js';
+
+const content = document.querySelector('#content');
+
+content.innerHTML = Home();
---
+++ src/navbar/home.js
@@ -0,0 +1,21 @@
+export default function Home() {
+
+    const html = `
+        <img src="./assets/img/header.svg">
+        <div class="welcome">
+            <h1>
+                <span>The Best Ramen</span>
+                <p>In The Town!</p>
+            </h1>
+            <p>
+                Having a rough day? Soften it with our delicious ramen.
+            </p>
+
+            <button>Reserve A Table</button>
+        </div>
+    `;
+
+    content.classList.add('home', 'flex');
+
+    return html;
+};
